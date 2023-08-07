c06f1ba4babab48ef2c0cbedc75a67c37fd55ee1
foylaou <s225002731@gmail.com>
2023-08-07 11:51:37 +0800

    linebot

--- app.py
+++ app.py
@@ -43,7 +43,14 @@ def handle_message(event):
         },
     ]
     test_message = TextSendMessage(text='''$ Master Finance $
-    Hello! 你好，你住哪裡?，我住高雄，約嗎?''')
+    Hello! 你好，你住哪裡?，我住高雄，約嗎?''',emoji=emoji)
+    sticker_message = StickerSendMessage(
+        package_id="8522",
+        sticker_id="16581271",
+        )
+    line_bot_api.reply_message(
+        event.reply_token,
+        [test_message,sticker_message])
     # message = TextSendMessage(text=event.message.text)
     # line_bot_api.reply_message(event.reply_token, message)
 
