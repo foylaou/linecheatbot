81abc86f44f84e1121eee8cb04ba7f56051798ee
foylaou <64564712+foylaou@users.noreply.github.com>
2023-08-07 10:54:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def callback():
 
 
 # 處理訊息
-@handler.add(MassageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
