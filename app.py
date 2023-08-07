0b0a38b5d7af8e339589eb8997e1b9cb529307f3
foylaou <s225002731@gmail.com>
2023-08-07 13:45:32 +0800

    linebot

--- app.py
+++ app.py
@@ -32,29 +32,6 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-def handle_message(event):
-    emoji = [
-        {
-            "index":0,
-            "productId": "5ac2213e040ab15980c9b447",
-            "emojiId": "002",
-        },
-        {
-            "index": 17,
-            "productId": "5ac2213e040ab15980c9b447",
-            "emojiId": "002",
-        },
-    ]
-    test_message = TextSendMessage(text='''$ Master Finance $ Hello! 你好，你住哪裡?我住高雄，約嗎?''',emoji=emoji)
-    sticker_message = StickerSendMessage(
-        package_id="8522",
-        sticker_id="16581271",
-        )
-    line_bot_api.reply_message(
-        event.reply_token,
-        [test_message,sticker_message])
-    # message = TextSendMessage(text=event.message.text)
-    # line_bot_api.reply_message(event.reply_token, message)
 
 
 if __name__ == "__main__":
--- events/basic.py
+++ events/basic.py
@@ -1,10 +1,26 @@
-from flask import Flask, request, abort
-from linebot import (LineBotApi, WebhookHandler, exceptions)
-from linebot.exceptions import (InvalidSignatureError)
-from linebot.models import *
+from line_bar_api import *
 
+def handle_message(event):
+    emoji = [
+        {
+            "index":0,
+            "productId": "5ac2213e040ab15980c9b447",
+            "emojiId": "002",
+        },
+        {
+            "index": 17,
+            "productId": "5ac2213e040ab15980c9b447",
+            "emojiId": "002",
+        },
+    ]
+    test_message = TextSendMessage(text='''$ Master Finance $ Hello! 你好，你住哪裡?我住高雄，約嗎?''',emoji=emoji)
+    sticker_message = StickerSendMessage(
+        package_id="8522",
+        sticker_id="16581271",
+        )
+    line_bot_api.reply_message(
+        event.reply_token,
+        [test_message,sticker_message])
+    # message = TextSendMessage(text=event.message.text)
+    # line_bot_api.reply_message(event.reply_token, message)
 
-# Channel access token
-line_bot_api = LineBotApi("jvP6prUhvT+cbHtN3B/cEgugl32WUAA1Oy1CD7FJE/vmcdv8wRgWAd+0lfi83dJEsEv6/2R8cDyJad5BF5F9Icrp++c9bzNjGzoxppc2kf6bJYio6Hk0r/zs1sVD4LW9C2v6lQC834wnP0I888kX6QdB04t89/1O/w1cDnyilFU=")
-# Channel secret
-handler = WebhookHandler("974df81e00a95ccd63aab56e02f62d52")
