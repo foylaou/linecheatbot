5515a3b5ca391bb8513965db6bcd48ba9eefee9c
foylaou <s225002731@gmail.com>
2023-08-07 17:03:16 +0800

    linebot

--- app.py
+++ app.py
@@ -4,29 +4,28 @@ from events.oil import *
 
 app = Flask(__name__)
 
-@handler.add(MessageEvent,message=TextMessage)
+
+@handler.add(MessageEvent, message=TextMessage)
 def handl_message(event):
     profile = line_bot_api.get_profile(event.score.user_id)
     uid = profile.user_id
     message_text = str(event.message.text).lower()
-
-    ####################使用說明 選單 油價查詢#######################
     if message_text == '@使用說明':
         about_us_event(event)
         Usage(event)
-
     if event.message.text == '想知道油價':
         content = oil_price()
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=content)
         )
-    ####################股票區#########################
     if event.message.text == '股價查詢':
         line_bot_api.push_message(
-        uid,
-        TextSendMessage("請輸入'#' + '股票代號'\n範例：#2330")
+            uid,
+            TextSendMessage("請輸入'#' + '股票代號'\n範例：#2330")
         )
+
+
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
