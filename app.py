ea2683322a0d4b1fe87cb58c0983f5ce81570f6b
foylaou <s225002731@gmail.com>
2023-08-07 13:51:26 +0800

    linebot

--- app.py
+++ app.py
@@ -1,37 +1,30 @@
-# LineBot所需套件
 from line_bot_api import *
 from events.basic import *
 
 app = Flask(__name__)
 
 
-@app.route("/callback", methods=["POST"])
+@app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.headers["X-Line-Signature"]
+    signature = request.headers['X-Line-Signature']
 
     body = request.get_data(as_text=True)
-    app.logger.info(f"Request body: {body}")
+    app.logger.info("Request body: " + body)
 
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        abort(400)
+        abort(100)
 
-    return "OK"
-
-
-# 處理訊息
-@handler.add(MessageEvent, message=TextMessage)
-def callback():
-    signature = request.headers['X-Line-Signature']
-
-    body = request.get_data(as_text = True)
-    app.logger.info("Request body: "+body)
-    try:
-        handler.handel(body, signature)
-    except InvalidSignatureError:
-        abort(400)
     return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message_text = str(event.message.text).lower()
+
+    if message_text == '@使用說明':
+        about_us_event(event)
 
 
 if __name__ == "__main__":
