c05e16ed9f38d75e7be2e6f363dcbd59fa5d6203
foylaou <64564712+foylaou@users.noreply.github.com>
2023-08-09 15:40:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -91,11 +91,16 @@ def handle_message(event):
         stockNumber = msg[2:6]
         content = write_my_stock(uid, user_name, stockNumber, msg[6:7], msg[7:])
         line_bot_api.push_message(uid, TextSendMessage(content))
-    else:
-        content = write_my_stock(uid, user_name, stockNumber, "未設定", "未設定")
-        line_bot_api.push_message(uid, TextSendMessage(content))
-        return 0
-    
+    # else:
+    #     content = write_my_stock(uid, user_name, stockNumber, "未設定", "未設定")
+    #     line_bot_api.push_message(uid, TextSendMessage(content))
+    #     return 0
+
+####
+    if re.match('幣別種類',msg):
+        message = show_Button()
+        line_bot_api.reply_message(event.reply_token,message)
+
 
 
     # ############"@小幫手"############
