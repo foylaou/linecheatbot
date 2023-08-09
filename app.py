d9803d82b77e5729523943e09fc6e442b2abbcca
foylaou <s225002731@gmail.com>
2023-08-09 13:34:49 +0800

    linebot

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def handle_message(event):
     message_text = str(event.message.text).lower()
     msg = str(event.message.text).upper().strip()
     emsg = event.message.text
-
+    user_name = profile.display_name
     # ############"使用說明"############
     if message_text == "@使用說明":
         about_us_event(event)
@@ -51,13 +51,22 @@ def handle_message(event):
         uid, 
         TextSendMessage("請輸入'#' + '股票代號'\n範例：#2330")
         )
-
-    #股價查詢
     if re.match("想知道股價[0-9]" , msg):
         stockNumber = msg[5:9]
         btn_msg = stock_reply_other(stockNumber)
         line_bot_api.push_message(uid , btn_msg)
         return 0
+    if re.mmatch('關注[0-9]{4}[<>][0-9]',msg):
+        twstockNumber = msg[2:6]
+        content = write_my_stock(uid,user_name,stockNumber,msg[6:7],msg[7:])
+        line_bot_api.push_message(uid,TextSendMessage(content))
+    else:
+        content = write_my_stock(uid,user_name,stockNumber,"未設定","未設定")
+        line_bot_api.push_message(uid, TextSendMessage(content))
+        return 0
+    #股價查詢
+
+
     
     if (emsg.startswith('#')):
         text = emsg[1:]
---
+++ model/pymongo.py
@@ -0,0 +1,39 @@
+from pymongo import MongoClient
+from datetime import datetime
+
+stockDB = 'mydb'
+dbname = "howard-good31"
+
+
+def constructor_stock():
+    client = MongoClient(
+        "mongodb://av2288444:<password>@ac-gbnruue-shard-00-00.kt0hhit.mongodb.net:27017,ac-gbnruue-shard-00-01.kt0hhit.mongodb.net:27017,ac-gbnruue-shard-00-02.kt0hhit.mongodb.net:27017/?ssl=true&replicaSet=atlas-r0k35s-shard-0&authSource=admin&retryWrites=true&w=majority")
+    db = client[stockDB]
+    return db
+
+
+def update_my_stock(user_name, stockNumber, condition, target_price):
+    db = constructor_stock()
+    collect = db[user_name]
+    collect.update_many({"favorite_stock": stockNumber}, {'$set': {'condition': condition, "price": target_price}})
+    content = f"股票{stockNumber}更新成功"
+    return content
+
+
+def write_my_stock(userID, user_name, stockNumber, condition, target_price):
+    db = constructor_stock()
+    collect = db[user_name]
+    is_exit = collect.find_one({'favorite_stock': stockNumber})
+    if is_exit != None:
+        content = update_my_stock(user_name, stockNumber, condition, target_price)
+        return content
+    else:
+        collect.insert_one({
+            "userID": userID,
+            "favorite_stock": stockNumber,
+            "condition": condition,
+            "price": target_price,
+            "tag": "stock",
+            "date_info": datetime.now()
+        })
+        return f"{stockNumber} 已新增您的股票清單"
