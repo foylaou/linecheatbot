3e7f6d807fc2185ae2a64b62f7bbeeb86a31fe50
foylaou <64564712+foylaou@users.noreply.github.com>
2023-08-11 16:36:09 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # linecheatbot
+
+
+before `app.py` please follow comment
+```commandline
+pip install -r requirements.txt
+```
--- app.py
+++ app.py
@@ -2,13 +2,27 @@ from line_bot_api import *
 from events.basic import *
 from events.oil import *
 from events.Msg_Template import *
-from model.mongodb import *
+from model.pymongo import *
 from events.EXRate import *
 import re
 import twstock
 import datetime
-
+import requests
+from bs4 import BeautifulSoup
 app = Flask(__name__)
+
+
+# 抓取使用者關心的股票
+def cache_users_stock():
+    db = constructor_stock()
+    nameList = db.list_collection_names()
+    users = []
+    for i in range(len(nameList)):
+        collect = db[nameList[i]]
+        cel = list(collect.find({"tag": 'stock'}))
+        users.append(cel)
+    return users
+
 
 @app.route("/callback", methods=["POST"])
 def callback():
@@ -17,13 +31,13 @@ def callback():
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
 
     return "OK"
+
 
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
@@ -35,7 +49,20 @@ def handle_message(event):
     message_text = str(event.message.text).lower()
     msg = str(event.message.text).upper().strip()
     emsg = event.message.text
-
+    #######ㄏ########
+    # if message_text == "user":
+    #     content = profile.user_id
+    #     line_bot_api.reply_message(
+    #         event.reply_token,
+    #         TextSendMessage(content)
+    #     )
+    #     print(profile.user_id)
+    ########孟和葛葛交的死邏輯要記住#######
+    if message_text == "user":
+        line_bot_api.push_message(
+            uid,
+            TextSendMessage(profile.user_id)
+        )
     # ############"使用說明"############
     if message_text == "@使用說明":
         about_us_event(event)
@@ -45,21 +72,36 @@ def handle_message(event):
     if message_text == "油價查詢":
         content = oil_price()
         line_bot_api.reply_message(
-        event.reply_token, 
-        TextSendMessage(content)
+            event.reply_token,
+            TextSendMessage(content)
         )
 
     # ############"股價查詢"############
     if message_text == "股價查詢":
         line_bot_api.push_message(
-        uid, 
-        TextSendMessage("請輸入'#' + '股票代號'\n範例：#2330")
+            uid,
+            TextSendMessage("請輸入'#' + '股票代號'\n範例：#2330")
         )
 
     if re.match("想知道股價[0-9]", msg):
         stockNumber = msg[-4:]
         btn_msg = stock_reply_other(stockNumber)
         line_bot_api.push_message(uid, btn_msg)
+        return 0
+    if re.match('股票清單', msg):
+        line_bot_api.push_message(uid, TextSendMessage('稍等一下，查詢中...'))
+        content = show_stock_setting(user_name, uid)
+        line_bot_api.push_message(uid, TextSendMessage(content))
+        return 0
+
+    # 刪除使用者特定的股票
+    if re.match('刪除[0-9]{4}', msg):
+        content = delete_my_stock(user_name, msg[2:])
+        line_bot_api.push_message(uid, TextSendMessage(content))
+        return 0
+    if re.match('清空股票', msg):
+        content = delete_my_allstock(user_name, uid)
+        line_bot_api.push_message(uid, TextSendMessage(content))
         return 0
     if (emsg.startswith('#')):
         text = emsg[1:]
@@ -70,7 +112,7 @@ def handle_message(event):
         except Exception as e:
             print(e)
 
-        my_datetime = datetime.datetime.fromtimestamp(stock_rt['timestamp']+8*60*60)
+        my_datetime = datetime.datetime.fromtimestamp(stock_rt['timestamp'] + 8 * 60 * 60)
         my_time = my_datetime.strftime("%H:%M:%S")
 
         content += f"{stock_rt['info']['name']} ({stock_rt['info']['code']}) {my_time}"
@@ -97,10 +139,14 @@ def handle_message(event):
     #     line_bot_api.push_message(uid, TextSendMessage(content))
     #     return 0
 
-####
-    if re.match('幣別種類',msg):
+    ##
+    if getCurrencyName(msg)[0] == True:
+        text = showCurrency(msg)
+        line_bot_api.push_message(uid, TextSendMessage(text))
+
+    if re.match('幣別種類', msg):
         message = show_Button()
-        line_bot_api.reply_message(event.reply_token,message)
+        line_bot_api.reply_message(event.reply_token, message)
 
     if re.match("換匯[A-Z]{3}/[A-Z]{3}/[0-9]", msg):
         line_bot_api.push_message(uid, TextSendMessage("正在為您計算..."))
@@ -111,21 +157,76 @@ def handle_message(event):
     if message_text == "@小幫手":
         button_template = Template_msg()
         line_bot_api.reply_message(
-        event.reply_token, button_template
+            event.reply_token, button_template
         )
+
+
+    if re.match('關閉提醒',msg):
+        import schedule
+        schedule.clear()
+        ######提醒######
+    if re.match("股價提醒", msg):
+        import schedule
+        import time
+
+        def look_stock_price(stock, condition, price, userID):
+            print(userID)
+            url = "https://tw.stock.yahoo.com/q/q?s=" + stock
+
+            list_req = requests.get(url)
+            soup = BeautifulSoup(list_req.content, "html.parser")
+            getstock = soup.findAll("span")[11].text
+            content = stock + "當前股價為: " + getstock
+
+            if condition == "<":
+                content += "\n篩選條件為: <" + price
+                if float(getstock) < float(price):
+                    content += "\n符合" + getstock + "<" + price + "的篩選條件"
+                    line_bot_api.push_message(uid, TextSendMessage(content))
+
+            elif condition == ">":
+                content += "\n篩選條件為: >" + price
+                if float(getstock) > float(price):
+                    content += "\n符合" + getstock + ">" + price + "的篩選條件"
+                    line_bot_api.push_message(uid, TextSendMessage(content))
+
+            elif condition == "=":
+                content += "\n篩選條件為: =" + price
+                if float(getstock) == float(price):
+                    content += "\n符合" + getstock + "=" + price + "的篩選條件"
+                    line_bot_api.push_message(uid, TextSendMessage(content))
+
+        def job():
+            print("HH")
+            dataList = cache_users_stock()
+
+            for i in range(len(dataList)):
+                for k in range(len(dataList[i])):
+                    look_stock_price(dataList[i][k]['favorite_stock'], dataList[i][k]['condition'],
+                                     dataList[i][k]['price'], dataList[i][k]['userID'])
+
+        schedule.every(30).seconds.do(job).tag("daily-tasks-stock" + uid, "second")
+        # schedule.every().hour.do(job)
+        # schedule.every().day.at("17:19").do(job)
+        # schedule.every().monday.do(job)
+        # schedule.every().wednesday.at("14:45").do(job)
+
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
 
 
 @handler.add(FollowEvent)
 def handle_follow(event):
     emojis = [
         {
-            "index": 0, 
-            "productId": "5ac21a18040ab15980c9b43e", 
+            "index": 0,
+            "productId": "5ac21a18040ab15980c9b43e",
             "emojiId": "009"
-        }, 
+        },
         {
-            "index": 16, 
-            "productId": "5ac21a18040ab15980c9b43e", 
+            "index": 16,
+            "productId": "5ac21a18040ab15980c9b43e",
             "emojiId": "014"
         }
     ]
@@ -139,8 +240,8 @@ def handle_follow(event):
 
     line_bot_api.reply_message(
         event.reply_token, welcome_message
-        )
-    
+    )
+
 
 @handler.add(UnfollowEvent)
 def hadle_unfollow(event):
